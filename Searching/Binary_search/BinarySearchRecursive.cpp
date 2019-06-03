02d429d340766d230a066431f6624a5434d21d9e
mxhoa <hoalsbk@gmail.com>
2019-06-04 01:35:37 +0700

    add basic search algs

--- Searching/Binary_search/BinarySearchRecursive.cpp
+++ Searching/Binary_search/BinarySearchRecursive.cpp
@@ -1,1 +1,28 @@
 #include <iostream>
+#include <vector>
+
+/*
+ * - Time Complexity: O(logN)
+ * - Space Complexity: O(logN)
+ */
+bool BinarySearchRecursive(std::vector<int> &data, int low, int high, int value)
+{
+    if (low > high)
+    {
+        return false;
+    }
+
+    int mid = low + (high - low) / 2; // To avoid the overflow
+    if (data[mid] == value)
+    {
+        return true;
+    }
+    else if (data[mid] < value)
+    {
+        return BinarySearchRecursive(data, mid + 1, high, value);
+    }
+    else
+    {
+        return BinarySearchRecursive(data, low, mid - 1, value);
+    }
+}
--- Searching/Linear_search/linearSearchSorted.cpp
+++ Searching/Linear_search/linearSearchSorted.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+#include <vector>
+
+/*
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ */
+bool linearSearchSorted(std::vector<int> &data, int value)
+{
+    int size = data.size();
+    for (int i = 0; i < size; i++)
+    {
+        if (value == data[i])
+        {
+            return true;
+        }
+        else if (value < data[i])
+        {
+            return false;
+        }
+    }
+    return false;
+}

be6106a17734def97f1d1555694dda6caf5066f2
mxhoa <hoalsbk@gmail.com>
2019-07-15 18:54:33 +0700

    Implement QuickSort using multithreading

--- Sorting/Comparison/QuickSort/QuickSort.cpp
+++ Sorting/Comparison/QuickSort/QuickSort.cpp
@@ -7,8 +7,9 @@ private:
 	std::vector<int> &m_dataset;
 
 private:
-	void swap(std::vector<int> &nums, int first, int sescond);
-	void quickSortUtil(std::vector<int> &nums, int lower, int upper);
+	int partition(std::vector<int> &nums, int lo, int hi);
+	void exch(std::vector<int> &nums, int i, int j);
+	void quickSortUtil(std::vector<int> &nums, int lo, int hi);
 
 public:
 	QuickSort(std::vector<int> &data);
@@ -26,59 +27,52 @@ void QuickSort::sort() {
 void QuickSort::print() {
 	int size = m_dataset.size();
 
-	std::cout << "+++ NUMS +++++\n";
+	std::cout << "++++ NUMS +++++\n";
 	for (int i = 0; i < size; i++)
 	{
-		std::cout << m_dataset.at(i) << "\t";
+		std::cout << "->" << m_dataset.at(i);
 	}
-	std::cout << "\n+++ END ++++\n";
+	std::cout << std::endl;
 }
 
-void QuickSort::quickSortUtil(std::vector<int> &nums, int lower, int upper)
+void QuickSort::exch(std::vector<int> &nums, int i, int j)
 {
-	if (lower >= upper) return;
+	int tmp = nums[i];
+	nums[i] = nums[j];
+	nums[j] = tmp;
+}
 
-	int pivot = nums[lower];
-	int start = lower;
-	int stop  = upper;
-
-	while (lower < upper)
+int QuickSort::partition(std::vector<int> &nums, int lo, int hi)
+{
+	if (lo == hi) return lo;
+	int pivot = nums[lo];	// pivot is first element in nums
+	int i = lo + 1;
+	int j = hi;
+	
+	while (true)
 	{
-		while (nums[lower] <= pivot && lower < upper)
-		{
-			lower++;
-		}
-		while (nums[upper] > pivot && lower <= upper)
-		{
-			upper--;
-		}
-		if (lower < upper)
-		{
-			swap(nums, lower, upper);
-		}
+		while (nums[i] <= pivot && i < hi) i++;
+		while (nums[j] > pivot && j > lo) j--;
+		if (i >= j) break;
+		exch(nums, i, j);
 	}
-
-	swap(nums, upper, start);	//  upper is  the pivot position
-
-	quickSortUtil(nums, start, upper - 1); 	// pivot - 1 is the upper for left sub array
-	quickSortUtil(nums, upper + 1, stop);	// pivot + 1 is the lower for right sub array
-
+	exch(nums, lo, j);
+	return j;
 }
 
-void QuickSort::swap(std::vector<int> &nums, int first, int second)
+void QuickSort::quickSortUtil(std::vector<int> &nums, int lo, int hi)
 {
-	int temp = nums[first];
-	nums[first] = nums[second];
-	nums[second] = temp;
+	if (lo >= hi) return;
+	int s = partition(nums, lo, hi);
+	quickSortUtil(nums, lo, s - 1);
+	quickSortUtil(nums, s + 1, hi);
 }
 
 int main()
 {
-	std::vector<int> data = {3, 4, 2, 1, 6, 5, 8, 1, 1};
+	std::vector<int> data = {1 , 5, 3, 2, 8, 11, 20, 17, 14, 15, 2, 1, 9, 10};
 	QuickSort *qs = new QuickSort(data);
 	qs->sort();
-
 	qs->print();
-
 	return 0; 
 }

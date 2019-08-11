479f5591512e59e467a2b32a5c978887513b9803
mxhoa <hoalsbk@gmail.com>
2019-08-12 01:05:13 +0700

    Subset Sum Problem, added Graph

---
+++ DynamicProgramming/isSubsetSum.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class RecursiveSolution
+{
+public:
+    bool isSubsetUtil(std::vector<int> &nums, int len, int sum)
+    {
+        if (sum == 0)
+            return true;
+        if (len == 0 && sum != 0)
+            return false;
+
+        if (nums[len - 1] > sum)
+            return isSubsetUtil(nums, len - 1, sum);
+
+        return isSubsetUtil(nums, len - 1, sum) ||
+               isSubsetUtil(nums, len - 1, sum - nums[len - 1]);
+    }
+
+    bool isSubsetSum(std::vector<int> &nums, int sum)
+    {
+        int len = nums.size();
+
+        return isSubsetUtil(nums, len, sum);
+    }
+};
+
+/* 
+ *  ##[i][j] | 0   1   2   3 ... sum
+ *       [0] | T   F   F   F      F
+ *       [1] | T   
+ *       [2] | T
+ *       ... | T
+ *      [n-1]| T
+ *       [n] | T                  ?
+ * 
+ */
+class DPSolution
+{
+public:
+    bool isSubsetSum(std::vector<int> &nums, int sum)
+    {
+        int len = nums.size();
+
+        return isSubsetUtil(nums, len, sum);
+    }
+
+    bool isSubsetUtil(std::vector<int> &nums, int n, int sum)
+    {
+        // The value of subset[i][j] will be true if there is
+        // a subset of set[0..j-1] with sum equal to i
+        bool subset[n + 1][sum + 1];
+
+        // If sum is 0, then answer is TRUE
+        for (int i = 0; i <= n; i++)
+            subset[i][0] = true;
+
+        // If sum is not 0 and set is empty, then answer is false
+        for (int i = 1; i <= sum; i++)
+            subset[0][i] = false;
+
+        // Fill the subset table in bottom up manner
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= sum; j++)
+            {
+                if (j < nums[i - 1])
+                    subset[i][j] = subset[i - 1][j];
+
+                if (j >= nums[i - 1])
+                    subset[i][j] = subset[i - 1][j] || subset[i - 1][j - nums[i - 1]];
+            }
+        }
+
+        return subset[n][sum];
+    }
+};
+
+int main()
+{
+    std::vector<int> nums = {3, 34, 4, 12, 5, 2};
+    int sum = 9;
+
+    cout << "Result = " << (new RecursiveSolution())->isSubsetSum(nums, sum) << std::endl;
+    cout << "Result = " << (new DPSolution())->isSubsetSum(nums, sum) << std::endl;
+
+    return 0;
+}
--- Sorting/Comparison/HeapSort/HeapSort.cpp
+++ Sorting/Comparison/HeapSort/HeapSort.cpp
@@ -1,1 +1,84 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+
+class HeapSort
+{
+private:
+    std::vector<int> &m_dataset;
+
+private:
+    void heapify(std::vector<int> &data, int n, int i);
+    void sortUtil(std::vector<int> &data, int n);
+
+public:
+    HeapSort(std::vector<int> &data);
+    void execute();
+    void printList();
+};
+
+HeapSort::HeapSort(std::vector<int> &data) : m_dataset(data) {}
+
+void HeapSort::heapify(std::vector<int> &data, int n, int i)
+{
+    // Find the largest among root, left child and right child
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && data[l] > data[largest])
+        largest = l;
+    if (r < n && data[r] > data[largest])
+        largest = r;
+
+    // Swap and continue heapifying if root is not largest
+    if (largest != i)
+    {
+        std::swap(data[i], data[largest]);
+        heapify(data, n, largest);
+    }
+}
+
+void HeapSort::sortUtil(std::vector<int> &data, int n)
+{
+    // Build max heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(data, n, i);
+    }
+
+    // Heap sort
+    for (int i = n - 1; i >= 0; i--)
+    {
+        std::swap(data[0], data[i]);
+
+        // Heapify root element to get highest element at root again
+        heapify(data, i, 0);
+    }
+}
+
+void HeapSort::execute()
+{
+    int n = m_dataset.size();
+    sortUtil(m_dataset, n);
+}
+
+void HeapSort::printList()
+{
+    for (auto i : m_dataset)
+    {
+        std::cout << i << '\t';
+    }
+    std::cout << "\n";
+}
+
+int main()
+{
+    std::vector<int> nums = {9, 4, 5, 1, 0, 4, 3, 8, 2, 6, 7};
+    HeapSort *hs = new HeapSort(nums);
+    hs->printList();
+    hs->execute();
+    hs->printList();
+
+    return 0;
+}
